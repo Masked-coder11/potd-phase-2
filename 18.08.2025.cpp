53c48830b2f4e35411a6d796d23995a2860ba26a
Masked-coder11 <153896221+Masked-coder11@users.noreply.github.com>
2025-08-18 00:23:02 +0530

    Create 18.08.2025.cpp

---
+++ 18.08.2025.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int ans=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int num= n-i;
+            ans=max(ans, min(num, arr[i]));
+        }
+        return ans;
+        
+        
+    }
+};
